75ac6248348382ecb794f146bf65850ba6678028
ezfx44 <nickich161@gmail.com>
2017-05-01 16:09:23 +0300

    Add files via upload

---
+++ include/quick_sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "quick_sort.h"
+using namespace std;
+
+template <typename Iterator>
+void quick_sort(Iterator, Iterator);
+
+int main()
+{
+	srand(time(0));
+	vector <int> v;
+	for (int i = 0; i < 10; i++)
+	{
+		v.push_back(rand() % 10);
+	}
+	cout << "Unsorted vector:\t";
+	for (int i = 0; i < 10; i++)
+	{
+		cout << v[i];
+	}
+	cout << "\nSorted vector: \t\t";
+	quick_sort(v.begin(), v.end());
+	for (int i = 0; i < 10; i++)
+	{
+		cout << v[i];
+	}
+	cin.get();
+	return 0;
+}
